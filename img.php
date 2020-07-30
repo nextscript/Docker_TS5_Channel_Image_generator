daf449f4d4f63860ba58292602a989f89488aee3
nextscript <stavros.d@hotmail.de>
2020-07-30 17:32:37 +0200

    Update img.php

--- img.php
+++ img.php
@@ -1,22 +1,32 @@
 <?PHP
-	header("Content-type: image/png");
+$tmp = explode('.', $_GET['img']);
+$endung = array_pop($tmp);
+
+switch( $endung ) {
+    case "png": $ctype="image/png"; break;
+    case "jpeg":
+    case "jpg": $ctype="image/jpeg"; break;
+    default:
+}
+
+switch( $endung ) {
+    case "png": $view=$image = imagecreatefrompng ( $_GET['img'] );; break;
+    case "jpeg":
+    case "jpg": $view=$image = imagecreatefromjpeg ( $_GET['img'] );; break;
+    default:
+}
+
+	header("Content-type:". $ctype);
 	if($_GET['sub'] != ''){
 		$submax = 10 * $_GET['sub'];
-		$image = imagecreatefrompng ( $_GET['img'] );
+		$view;
 		$image1 = imagecreatetruecolor ( 960, 38 );
-		
-		imagesavealpha($image1, true);
-		
-		$color = imagecolorallocatealpha($image1, 0, 0, 0, 127);
-		
-		imagefill($image1, 0, 0, $color);
-		imagepng($image1, "test.png");
 		ImageCopy ( $image1, $image, -$submax, 0, 0, $_GET['i'], 960, 38);
-		imagepng( $image1 );
+		imagejpeg( $image1 );
 	}else{
-		$image = imagecreatefrompng ( $_GET['img'] );
+		$view;
 		$image1 = imagecreatetruecolor ( 960, 38 );
 		ImageCopy ( $image1, $image, 0, 0, 0, $_GET['i'], 960, 38 );
-		imagepng( $image1 );
+		imagejpeg( $image1 );
 	}
 ?>
